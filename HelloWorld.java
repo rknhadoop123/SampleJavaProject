93f214378d92edcf1bf29961c0f9f95ae484bf45
rknhadoop123 <43198229+rknhadoop123@users.noreply.github.com>
2019-02-02 20:02:13 +0530

    Update HelloWorld.java
    second update in helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,8 @@ public static void main(String as[])
 for(int i=1;i<=10;i++)
 {
 System.out.println("HelloWorld: "+i);
-  System.out.println("updated from github repo");
+  System.out.println("updated from github reepo");
+  System.out.println("second updated");
 }
 }
 }
